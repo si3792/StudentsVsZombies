cfe8ca1dfeebd997d26f0e0b4c34fe01fae41635
si3792 <si3792@gmx.com>
2015-10-17 14:25:15 -0700

    GraveRandomizer added

---
+++ Assets/Scripts/GraveRandomizerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraveRandomizerControl : MonoBehaviour {
+
+    int winTomb;
+
+    int GetEmptyGrave(GameObject[] tombs)
+    {
+        int r, tries = 200;
+        do {
+            r = (int)(Random.value * 10000) % tombs.Length;
+            tries--;
+        }
+        while (tombs[r].GetComponent<graveControl>().empty == false && tries > 0);
+
+        return r;
+    }
+	
+	void Start () {
+
+        GameObject[] tombs = GameObject.FindGameObjectsWithTag("graveArea");
+
+        // Set win grave
+        winTomb = GetEmptyGrave(tombs);
+        tombs[winTomb].GetComponent<graveControl>().empty = false;
+        tombs[winTomb].GetComponent<graveControl>().graveLoot = graveControl.loot.WIN;
+
+    }
+	
+
+}
