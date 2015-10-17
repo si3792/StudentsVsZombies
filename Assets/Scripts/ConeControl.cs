1149a5928e54e537f3583c037a7c616b6315d266
si3792 <si3792@gmx.com>
2015-10-17 13:43:53 -0700

    Added beni background

---
+++ Assets/Scripts/ConeControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConeControl : MonoBehaviour {
+
+    public float damage = 200;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
