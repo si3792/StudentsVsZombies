debef860482f619de90f49cbbe234b30afb614e7
si3792 <si3792@gmx.com>
2015-10-17 23:59:03 -0700

    Added spark effects

---
+++ Assets/Scripts/Spark.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spark : MonoBehaviour {
+    public float min = 2f;
+    public float max = 4f;
+	void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+        float a = Random.RandomRange(min, max);
+        this.gameObject.transform.localScale *= a;
+ 
+    }
+
+}
---
+++ Assets/Scripts/StickToTagged.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToTagged : MonoBehaviour {
+
+    public string tag;
+
+	void Update () {
+        this.transform.position = GameObject.FindGameObjectWithTag(tag).transform.position;
+	}
+}
