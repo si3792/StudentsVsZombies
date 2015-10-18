30621ba390035e16446ca64fc8792b85f3fb0e1c
si3792 <si3792@gmx.com>
2015-10-17 22:50:51 -0700

    Alpha bonus

--- Assets/PlayerAnimScript.cs
+++ Assets/PlayerAnimScript.cs
@@ -12,12 +12,39 @@ public class PlayerAnimScript : MonoBehaviour {
         go.GetComponent<ShootShotgun>().Shoot(pl.GetComponent<PlayerMovement>().side );
     }
 
+    /*
+    // calcs alpha for maximum shine
+    public float bonus = 1f;
+    float cur = 1;
+    bool add = true;
+    public float bonusStep = 0.001f;
+    float formula(float hp)
+    {
+        if (hp < 200) return 1f;
+        if (hp < 50) return 0.6f;
+
+        if (cur > 1 + bonus && add == true)
+        {
+            add = false;
+        }
+        else if (cur < 1 && add == false)
+        {
+            add = true;
+        }
+        else if (add == true)
+        {
+            cur += bonusStep;
+        }
+        else cur -= bonusStep;
+
+        return cur;
+    }*/
 
     void Update()
     {
         GameObject go = GameObject.FindGameObjectWithTag("Player");
         float hp = go.GetComponent<PlayerMovement>().health;
         this.gameObject.GetComponent<SpriteRenderer>().material.color = new Color(this.gameObject.GetComponent<SpriteRenderer>().material.color.r, this.gameObject.GetComponent<SpriteRenderer>().material.color.g,
-            this.gameObject.GetComponent<SpriteRenderer>().material.color.b, hp / 400);
+            this.gameObject.GetComponent<SpriteRenderer>().material.color.b, formula(hp)  );
     }
 }
