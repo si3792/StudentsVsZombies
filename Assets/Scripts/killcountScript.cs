158eddf9125b489b10755eb5466736d67c64de44
si3792 <si3792@gmx.com>
2015-10-27 17:22:04 -0700

    Killcount added

---
+++ Assets/Scripts/killcountScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class killcountScript : MonoBehaviour {
+
+	
+	void Update () {
+        this.GetComponent<Text>().text =  GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().killcount.ToString();
+	}
+}
