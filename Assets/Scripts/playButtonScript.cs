d0f1a626315ac478b8e05177c679af4d87207ce3
si3792 <si3792@gmx.com>
2015-10-27 19:29:20 -0700

    item damage increased, play button improved

---
+++ Assets/Scripts/playButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = GameObject.FindGameObjectWithTag("tagMag").GetComponent<LevelChangerController>().selected;
+
+	}
+}
